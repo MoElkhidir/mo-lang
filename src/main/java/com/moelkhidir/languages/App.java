f3141552c53b577b9abab8575e516f4f7dd9e463
Mo Elkhidir <mhharktat@gmail.com>
2023-02-06 22:44:48 +0100

    initial commit

---
+++ src/main/java/com/moelkhidir/languages/App.java
@@ -0,0 +1,13 @@
+package com.moelkhidir.languages;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
