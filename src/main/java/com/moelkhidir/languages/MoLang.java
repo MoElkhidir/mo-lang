1dc417e9cc4bad36f78214af39aec866f78e3ea3
Mo Elkhidir <mhharktat@gmail.com>
2023-02-06 23:53:53 +0100

    Added: 1. TokenType enum to define the types of token that we might parse in the source code. 2. Token which groups a lexeme with some meta data related to it.

--- src/main/java/com/moelkhidir/languages/MoLang.java
+++ src/main/java/com/moelkhidir/languages/MoLang.java
@@ -1,11 +1,13 @@
 package com.moelkhidir.languages;
 
+import com.moelkhidir.languages.core.Token;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 /** Hello world! */
 public class MoLang {
